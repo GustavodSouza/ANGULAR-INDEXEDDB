d6924e86f77ccf7def3e7e34a463da8869b988ed
Gustavo de Souza <gustavo.souza@meta.com.br>
2021-03-04 15:58:39 -0300

    Adicionado PrimeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PaginaInicialComponent } from './páginas/pagina-inicial/pagina-inicial.component';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ButtonModule,
+    InputTextModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
